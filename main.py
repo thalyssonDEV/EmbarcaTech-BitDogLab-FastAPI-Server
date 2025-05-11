98f0edd8e3daeadff9823bb703ac8e97c21893d8
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2025-05-10 21:28:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ app = FastAPI()
 
 last_data = {}
 
-# Montar a pasta de arquivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# Configuração do motor de templates
 templates = Jinja2Templates(directory="templates")
 
 
@@ -23,26 +19,23 @@ async def receber_dados(request: Request):
     global last_data
 
     if request.method == "POST":
-        # Recebe os dados no formato JSON
+
         body = await request.json()
         print("Dados Recebidos (POST):", body)
 
-        # Processa os dados recebidos e armazena em processed_data
         processed_data = {
             "direction": body.get("direction"),
             "vrx": body.get("vrx"),
             "vry": body.get("vry"),
-            "button_status": body.get("button_status")
+            "button_status": body.get("button_status"),
+            "sensor_status": body.get("sensor_status")
         }
 
-        # Armazena os dados processados na variável global last_data
         last_data = processed_data
 
-        # Retorna uma resposta indicando que os dados foram recebidos corretamente
         return {"status": "ok"}
     
-    else:  # Se for GET
-        # Retorna os dados mais recentes
+    else: 
         return JSONResponse(content=last_data)
 
 
@@ -51,8 +44,4 @@ async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
-    print(f"\033[95mServidor FastAPI rodando...\033[0m")  # Cor roxa para "Servidor FastAPI"
-    print(f"\033[93mAcesse a aplicação em:\033[0m")  # Cor amarela para o texto "Acesse"
-    print(f"\033[1m\033[94m  -> http://127.0.0.1:8000\033[0m\n")  # Link em negrito e azul
-
     uvicorn.run(app, host="0.0.0.0", port=8000)
